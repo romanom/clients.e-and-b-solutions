607be18ab19378430099b6d29d262acb915c2d0e
Aaron Sisler <asisler@corp.epnet.com>
2018-04-10 21:20:09 -0400

    Setting up react and revserver

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    },
+    devtool: 'cheap-module-eval-source-map',
+    entry: './src/app.js',
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }, {
+            test: /\.s?css$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        }]
+    },
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js',
+    }
+};
