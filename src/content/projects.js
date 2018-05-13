75e3c41d1851393604efe25f954a0e878742c8b8
Aaron Sisler <asisler@corp.epnet.com>
2018-05-13 15:21:51 -0400

    Adding in the 2 new projects

--- src/content/projects.js
+++ src/content/projects.js
@@ -1,14 +1,27 @@
 export const projects = [
     {
-        clickableImage: '/images/project1_mini.jpg',
+        clickableImage: '/images/project4_mini.jpg',
         client: 'E&B Solutions',
-        date: 'July 2016',
+        date: 'May 2018',
         images: [
-            { src: '/images/project1_img1.jpg', alt: 'Screenshot 1' },
-            { src: '/images/project1_img2.jpg', alt: 'Screenshot 2' }
+            { src: 'images/project4_img1.jpg', alt: 'Screenshot 1' },
+            { src: 'images/project4_img2.jpg', alt: 'Screenshot 2' }
         ],
-        information: 'Created a responsive website from scratch and hosted in the cloud.',
-        name: 'Website Design'
+        information: `Using React, a JavaScript library for building user interfaces, and Amazon Web Services, the E&B Solutions' site was completely rebuilt.`,
+        informationCont: `This allowed for faster page load times, theme consistency, and a strong improvement in the user's overall experience.`,
+        name: 'Website Innovation'
+    },
+    {
+        clickableImage: '/images/project3_mini.jpg',
+        client: 'Valley Love Farm',
+        date: 'March 2018',
+        images: [
+            { src: 'images/project3_img1.jpg', alt: 'Screenshot 1' },
+            { src: 'images/project3_img2.jpg', alt: 'Screenshot 2' }
+        ],
+        information: `After proven success with the first implementation through GoDaddy's Website Builder, Valley Love Farm (formerly Valley Love Organic) decided to further trust E&B Solutions with a full website implementation.`,
+        informationCont: `This required building their new website from the ground up and choosing a new theme that matched their services offered.  This also meant making sure to maintain their current site functionality and provide new features the client was excited to receive.`,
+        name: 'Website Implementation'
     },
     {
         clickableImage: '/images/project2_mini.jpg',
@@ -23,5 +36,16 @@ export const projects = [
         information: `Using GoDaddy\'s WebSite builder, updated the existing site to use a newer and responsive theme.`,
         informationCont: `Added new pages for new services offered by client. Client\'s site traffic increased significantly once they could promote their redesigned site.`,
         name: 'Website Modification'
+    },
+    {
+        clickableImage: '/images/project1_mini.jpg',
+        client: 'E&B Solutions',
+        date: 'July 2016',
+        images: [
+            { src: '/images/project1_img1.jpg', alt: 'Screenshot 1' },
+            { src: '/images/project1_img2.jpg', alt: 'Screenshot 2' }
+        ],
+        information: 'Created a responsive website from scratch and hosted in the cloud.',
+        name: 'Website Design'
     }
 ]
