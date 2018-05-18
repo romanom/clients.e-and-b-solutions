297419a070f0430c06f3fe366a3465901b6c1f3a
Aaron Sisler <asisler@corp.epnet.com>
2018-05-18 10:22:35 -0400

    Running through the fix on save for components

--- src/components/FAQPage.js
+++ src/components/FAQPage.js
@@ -17,12 +17,12 @@ export default class FAQPage extends React.Component {
             <div id="faq_page">
                 <div className="faq_content">
                     {faqs.map((faq, index) => (
-                            <FAQWidget
-                                key={index}
-                                question={faq.question}
-                                answer={faq.answer}
-                            />
-                        ))}
+                        <FAQWidget
+                            key={index}
+                            question={faq.question}
+                            answer={faq.answer}
+                        />
+                    ))}
                 </div>
             </div>
         );
