fe0efc97c3e87b2ef7249d6b9a3d7d035e8d43f9
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2019-03-26 16:18:10 -0400

    Update: Updating the navbar to work in both mobile and desktop.

--- src/components/core/Navbar.js
+++ src/components/core/Navbar.js
@@ -17,6 +17,72 @@ export default class Navbar extends React.Component {
   closeSideMenu = () => this.setState({ isNavOpen: false });
 
   render() {
+    const isNavOpenClass = this.state.isNavOpen ? "nav_menu__links-open" : "";
+    return (
+      <div className="nav_menu">
+        <div className="nav_menu__brand">
+          <Link to="/">{this.props.companyName}</Link>
+        </div>
+        <div className="nav_menu__toggle">
+          {!this.state.isNavOpen && (
+            <FontAwesomeIcon
+              icon={faBars}
+              size="4x"
+              className="favicon"
+              onClick={this.openSideMenu}
+            />
+          )}
+        </div>
+        <div className={`nav_menu__links ${isNavOpenClass}`}>
+          <div className="nav_menu__links-close">
+            <FontAwesomeIcon
+              icon={faTimes}
+              size="4x"
+              className="favicon"
+              onClick={this.closeSideMenu}
+            />
+          </div>
+          <Link
+            to="/"
+            className="nav_menu__links-link"
+            onClick={this.closeSideMenu}
+          >
+            Home
+          </Link>
+          <Link
+            to="/pricing"
+            className="nav_menu__links-link"
+            onClick={this.closeSideMenu}
+          >
+            Pricing
+          </Link>
+          <Link
+            to="/portfolio"
+            className="nav_menu__links-link"
+            onClick={this.closeSideMenu}
+          >
+            Portfolio
+          </Link>
+          <Link
+            to="/faq"
+            className="nav_menu__links-link"
+            onClick={this.closeSideMenu}
+          >
+            FAQ
+          </Link>
+          <Link
+            to="/contact"
+            className="nav_menu__links-link-contact"
+            onClick={this.closeSideMenu}
+          >
+            Contact
+          </Link>
+        </div>
+      </div>
+    );
+  }
+
+  render1() {
     const isNavOpenClass = this.state.isNavOpen ? "-open" : "";
     return (
       <div className="nav_menu">
