543e714a2978f7f5577d5f4771d00c68dfcc3c82
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2019-08-29 14:43:41 -0400

    Contact page and container ready.  Need to pull in atoms and create form

---
+++ src/components/contact-form/contact-form.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./contact-form.scss";
+
+const ContactForm = () => <div className="contact-form">ContactForm</div>;
+
+export default ContactForm;
--- src/containers/contact-container/contact-container.js
+++ src/containers/contact-container/contact-container.js
@@ -1,6 +1,15 @@
 import React from "react";
+import ContactForm from "../../components/contact-form";
+
 import "./contact-container.scss";
 
-const ContactContainer = () => <div className="contact-container">ContactContainer</div>;
+const ContactContainer = () => (
+  <div className="contact-container">
+    <div className="contact-container__form">
+      <div className="contact-container__title">Send us a message</div>
+      <ContactForm />
+    </div>
+  </div>
+);
 
 export default ContactContainer;
