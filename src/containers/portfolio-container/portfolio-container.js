36704fc5aa8aaa11b4b1eb8f618b1bb6a9550c52
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2019-09-06 10:19:46 -0400

    Adding in the work for portfolio pictures

--- src/containers/portfolio-container/portfolio-container.js
+++ src/containers/portfolio-container/portfolio-container.js
@@ -1,7 +1,23 @@
 import React from "react";
+import Link from "next/link";
+import projects from "../../content/projects";
 
 import "./portfolio-container.scss";
 
-const PortfolioContainer = () => <div className="portfolio-container">PortfolioContainer</div>;
+const PortfolioContainer = () => (
+  <div className="portfolio-container">
+    {projects.map(project => (
+      <Link key={project.key} href={`/project?projectId=${project.key}`}>
+        <a className="portfolio-container__project">
+          <img
+            alt={project.thumbnail.alt}
+            className="portfolio-container__project-thumbnail"
+            src={project.thumbnail.src}
+          />
+        </a>
+      </Link>
+    ))}
+  </div>
+);
 
 export default PortfolioContainer;
--- src/content/projects.js
+++ src/content/projects.js
@@ -3,33 +3,41 @@ import { imagesFolderPath } from "../config";
 /* eslint-disable max-len */
 const projects = [
   {
-    clickableImage: `${imagesFolderPath}/project4_mini.jpg`,
     client: "E&B Solutions",
     date: "May 2018",
+    key: "project4",
     images: [
       { src: "/images/project4_img1.jpg", alt: "Screenshot 1" },
       { src: "/images/project4_img2.jpg", alt: "Screenshot 2" }
     ],
     information: `Using React, a JavaScript library for building user interfaces, and Amazon Web Services, the E&B Solutions' site was completely rebuilt.`,
     informationCont: `This allowed for faster page load times, theme consistency, and a strong improvement in the user's overall experience.`,
-    title: "Website Innovation"
+    title: "Website Innovation",
+    thumbnail: {
+      alt: "E&B Solutions",
+      src: `${imagesFolderPath}/project4_thumbnail.jpg`
+    }
   },
   {
-    clickableImage: "/images/project3_mini.jpg",
     client: "Valley Love Farm",
     date: "March 2018",
+    key: "project3",
     images: [
       { src: "/images/project3_img1.jpg", alt: "Screenshot 1" },
       { src: "/images/project3_img2.jpg", alt: "Screenshot 2" }
     ],
     information: `After proven success with the first implementation through GoDaddy's Website Builder, Valley Love Farm (formerly Valley Love Organic) decided to further trust E&B Solutions with a full website implementation.`,
     informationCont: `This required building their new website from the ground up and choosing a new theme that matched their services offered.  This also meant making sure to maintain their current site functionality and provide new features the client was excited to receive.`,
-    title: "Website Implementation"
+    title: "Website Implementation",
+    thumbnail: {
+      alt: "Valley Love Farm",
+      src: `${imagesFolderPath}/project3_thumbnail.jpg`
+    }
   },
   {
-    clickableImage: "/images/project2_mini.jpg",
     client: "Valley Love Organic",
     date: "September 2016",
+    key: "project2",
     images: [
       { src: "/images/project2_img1.jpg", alt: "Screenshot 1" },
       { src: "/images/project2_img2.jpg", alt: "Screenshot 2" },
@@ -38,19 +46,27 @@ const projects = [
     ],
     information: `Using GoDaddy's WebSite builder, updated the existing site to use a newer and responsive theme.`,
     informationCont: `Added new pages for new services offered by client. Client's site traffic increased significantly once they could promote their redesigned site.`,
-    title: "Website Modification"
+    title: "Website Enhancement",
+    thumbnail: {
+      alt: "Valley Love Organic",
+      src: `${imagesFolderPath}/project2_thumbnail.jpg`
+    }
   },
   {
-    clickableImage: "/images/project1_mini.jpg",
     client: "E&B Solutions",
     date: "July 2016",
+    key: "project1",
     images: [
       { src: "/images/project1_img1.jpg", alt: "Screenshot 1" },
       { src: "/images/project1_img2.jpg", alt: "Screenshot 2" }
     ],
     information:
       "Created a responsive website from scratch and hosted in the cloud.",
-    title: "Website Design"
+    title: "Website Design",
+    thumbnail: {
+      alt: "E&B Solutions",
+      src: `${imagesFolderPath}/project1_thumbnail.jpg`
+    }
   }
 ];
 
